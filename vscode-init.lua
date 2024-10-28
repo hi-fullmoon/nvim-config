0774cc0af67969c385fc57971eea4de89a97381f
zhengbiwen <1317812522@qq.com>
2024-10-28 13:51:18 +0800

    Update

--- vscode-init.lua
+++ vscode-init.lua
@@ -63,6 +63,27 @@ vim.keymap.set('n', '<leader>]', function() vscode.call('editor.action.marker.ne
 vim.keymap.set('n', '[c', function() vscode.call('editor.action.dirtydiff.previous') end)
 vim.keymap.set('n', ']c', function() vscode.call('editor.action.dirtydiff.next') end)
 
+local function moveCursor(direction)
+  if (vim.fn.reg_recording() == '' and vim.fn.reg_executing() == '') then
+    return ('g' .. direction)
+  else
+    return direction
+  end
+end
+
+vim.keymap.set('n', 'k', function()
+  return moveCursor('k')
+end, {
+  expr = true,
+  remap = true
+})
+vim.keymap.set('n', 'j', function()
+  return moveCursor('j')
+end, {
+  expr = true,
+  remap = true
+})
+
 -- 加载 neovim 插件
 ------------------------------------------------------------------------------------------
 require('lazy').setup('plugins')
