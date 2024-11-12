706dcc5b927821a35dba77aa616e9fd877b94a6d
zhengbiwen <1317812522@qq.com>
2024-11-12 11:37:39 +0800

    fix: update

--- lua/command.lua
+++ lua/command.lua
@@ -1,7 +1,19 @@
--- 中文输入法切换问题
-vim.cmd [[
-  autocmd InsertLeave * :silent !/usr/local/bin/macism com.apple.keylayout.ABC
-]]
+-- 检测操作系统和芯片架构
+if vim.fn.has('mac') == 1 then
+  -- 检测是否为 Apple Silicon
+  local is_apple_silicon = vim.fn.system('uname -m'):find('arm64') ~= nil
+  local macism_path = is_apple_silicon
+    and '/opt/homebrew/bin/macism'  -- Apple Silicon 路径
+    or '/usr/local/bin/macism'      -- Intel 路径
+
+  vim.cmd(string.format([[
+    autocmd InsertLeave * :silent !%s com.apple.keylayout.ABC
+  ]], macism_path))
+elseif vim.fn.has('win32') == 1 then
+  vim.cmd [[
+    autocmd InsertLeave * :silent !im-select 1033
+  ]]
+end
 
 -- 复制高亮
 vim.cmd [[
--- vscode-init.lua
+++ vscode-init.lua
@@ -18,9 +18,22 @@ vim.o.clipboard = 'unnamedplus'
 
 -- 一些命令
 ------------------------------------------------------------------------------------------
-vim.cmd [[
-  autocmd InsertLeave * :silent !/usr/local/bin/macism com.apple.keylayout.ABC
-]]
+-- 根据操作系统设置不同的输入法切换命令
+if vim.fn.has('mac') == 1 then
+  -- 检查是否为 Apple Silicon
+  local is_apple_silicon = vim.fn.system('uname -m'):find('arm64') ~= nil
+  local macism_path = is_apple_silicon
+    and '/opt/homebrew/bin/macism'  -- Apple Silicon 路径
+    and '/usr/local/bin/macism'     -- Intel Mac 路径
+
+  vim.cmd(string.format([[
+    autocmd InsertLeave * :silent !%s com.apple.keylayout.ABC
+  ]], macism_path))
+elseif vim.fn.has('win32') == 1 then
+  vim.cmd [[
+    autocmd InsertLeave * :silent !im-select 1033
+  ]]
+end
 
 vim.cmd [[
   augroup YankHighlight
@@ -34,12 +47,15 @@ vim.cmd [[
 local vscode = require('vscode')
 
 vim.keymap.set('n', 'K', function() vscode.action('editor.action.showHover') end)
-vim.keymap.set('n', 'W', function() vscode.action('workbench.action.files.save') end)
+vim.keymap.set('n', 'W', function() vscode.action('saveAll') end)
 vim.keymap.set('n', 'Q', function() vscode.call('workbench.action.closeActiveEditor') end)
 vim.keymap.set('n', 'tt', function() vscode.call('workbench.explorer.fileView.focus') end)
 vim.keymap.set('n', '<leader>be', function() vscode.call('workbench.action.showAllEditors') end)
 
 vim.keymap.set('n', '<leader>be', function() vscode.call('workbench.action.showAllEditors') end)
+
+vim.keymap.set('n', '<C>+o', function() vscode.call('workbench.action.navigateBack') end)
+vim.keymap.set('n', '<C>+i', function() vscode.call('workbench.action.navigateForward') end)
 
 vim.keymap.set('n', '<leader>1', function() vscode.call('workbench.action.openEditorAtIndex1') end)
 vim.keymap.set('n', '<leader>2', function() vscode.call('workbench.action.openEditorAtIndex2') end)
@@ -62,27 +78,6 @@ vim.keymap.set('n', '<leader>[', function() vscode.call('editor.action.marker.pr
 vim.keymap.set('n', '<leader>]', function() vscode.call('editor.action.marker.next') end)
 vim.keymap.set('n', '[c', function() vscode.call('editor.action.dirtydiff.previous') end)
 vim.keymap.set('n', ']c', function() vscode.call('editor.action.dirtydiff.next') end)
-
-local function moveCursor(direction)
-  if (vim.fn.reg_recording() == '' and vim.fn.reg_executing() == '') then
-    return ('g' .. direction)
-  else
-    return direction
-  end
-end
-
-vim.keymap.set('n', 'k', function()
-  return moveCursor('k')
-end, {
-  expr = true,
-  remap = true
-})
-vim.keymap.set('n', 'j', function()
-  return moveCursor('j')
-end, {
-  expr = true,
-  remap = true
-})
 
 -- 加载 neovim 插件
 ------------------------------------------------------------------------------------------
